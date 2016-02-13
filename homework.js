db10fd913867c59dd32f3721cc782fcc252b011e
Marianne Louise Rivera <marianneluching@gmail.com>
2016-02-14 05:17:08 +0800

    Finished with all the specs

--- homework.js
+++ homework.js
@@ -2,6 +2,9 @@ var userInput = 123234;
 var delim = ",";
 var jumpSize = 3;
 
+var stringInput = "one hundred twenty three thousand two hundred thirty four";
+var stringCurrency = "PHP";
+
 function numToWords(input){
 	var ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 	var tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
@@ -93,7 +96,56 @@ function numToWords(input){
 }
 
 function wordsToNum(input){
+	var ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	var tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	var places = ['hundred', 'thousand', 'million'];
+	var additional = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
+	var number = 0;
+	var finalNumber = 0;
+
+	input = input.toString().split(" ");
+	for(var i = 0; i < input.length; i++){
+
+		for(var w = 0; w < ones.length; w++){
+			if(input[i] == ones[w]){
+				number += w;
+			}
+		}
+
+		for(var x = 0; x < tens.length; x++){
+			if(input[i] == tens[x]){
+				x = x * 10;
+				number += x;
+			}
+		}
+
+		for(var y = 0; y < additional.length; y++){
+			if(input[i] == additional[y]){
+				y = 10 + y;
+				number += y;
+			}
+		}
+
+		for(var z = 0; z < places.length; z++){
+			if(input[i] == places[z]){
+				if(z == 0) number = number * 100;
+				else if(z == 1){
+					number = number * 1000;
+					finalNumber += number;
+					number = 0;
+				}
+				else if(z == 2){
+					number = number * 1000000;
+					finalNumber += number;
+					number = 0;
+				}
+			}
+		}
+	}
+	finalNumber += number;
+	//console.log(finalNumber);
+	return finalNumber;
 }
 
 function numberDelimited(input, delim, jumpSize){
@@ -112,5 +164,12 @@ function numberDelimited(input, delim, jumpSize){
 	console.log(toPrint);
 }
 
-numberDelimited(userInput, delim, jumpSize);
+function wordsToCurrency(input, currency){
+	var toPrint = currency + wordsToNum(input);
+	console.log(toPrint);
+}
+
+//numberDelimited(userInput, delim, jumpSize);
 //numToWords(userInput);
+console.log(wordsToNum(stringInput));
+//wordsToCurrency(stringInput, stringCurrency);
